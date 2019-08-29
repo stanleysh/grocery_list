dcbf202e91b412cfdc69563e6bf994a74c211d05
Stanley Sham <stanley.sham94@gmail.com>
2019-08-29 11:08:29 -0400

    completed assignment, fixed div issue

--- src/GroceryType.js
+++ src/GroceryType.js
@@ -5,18 +5,28 @@ class GroceryType extends React.Component {
 
     state = {
         numberOf: this.props.amount,
-        groceryType: this.props.groceryType,
-        groceryItem: this.props.groceryItem
     };
 
-    setAmount = (v) => {
-        this.setState({ numberOf:v})
+    increaseAmount = () => {
+        this.setState({ numberOf: this.state.numberOf + 1})
     };
+
+    decreaseAmount = () => {
+        if (this.state.numberOf == 0){
+            console.log('Too low')
+        } else {
+        this.setState({ numberOf: this.state.numberOf -1})
+        }
+    }
 
     render() {
         return (
-            <li className = {this.props.groceryType}>
-                <GroceryItem groceryItem = {this.props.groceryItem} handleValueChange = {this.setAmount} value = {this.state.numberOf}/>
+            <li className = {this.props.groceryType} id = { this.props.groceryItem }>
+                <button type="button" className="decrease" onClick={ this.decreaseAmount }>-</button>
+                {/* <input type="text" className="txt" value={value} onChange = { (event) => { updateValue(event.target.value) } }/> */}
+                <span>{this.state.numberOf} {this.props.groceryItem}</span>
+                <button type="button" className="increase" onClick={ this.increaseAmount }>+</button>
+                {/* <GroceryItem groceryItem = {this.props.groceryItem}  value = {this.state.numberOf}/> */}
             </li>
         )
     }
